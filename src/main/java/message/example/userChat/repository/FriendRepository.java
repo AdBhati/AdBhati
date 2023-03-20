597708702bc073e8e13f47be3c3613eb876d791a
Divyanshu Bhati <dbhati1021@gmail.com>
2023-03-20 17:45:36 +0530

    Test1.1

--- src/main/java/message/example/userChat/repository/FriendRepository.java
+++ src/main/java/message/example/userChat/repository/FriendRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.Query;
 import message.example.userChat.model.Friend;
 
 public interface FriendRepository extends JpaRepository<Friend, UUID> {
-
+    // Test
     @Query(value = "Select * from friend where user_id = ?1 AND id=?2", nativeQuery = true)
     Friend findByUserIdAndFriend(UUID userId, UUID friendId);
 
